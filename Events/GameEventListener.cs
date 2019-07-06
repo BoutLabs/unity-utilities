1d2b4eb440e4f1d5a6c6b8fa1094014443f0a632
Jesse Curry <jesse@jessecurry.net>
2019-07-06 19:07:22 -0400

    Add GameEvent and GameEventListener

---
+++ Events/GameEventListener.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace BoutLabs
+{
+    public class GameEventListener : MonoBehaviour
+    {
+        [Tooltip("Event to respond to.")]
+        public GameEvent Event;
+
+        [Tooltip("Response to invoke when the Event is raised.")]
+        public UnityEvent Response;
+
+        private void OnEnable()
+        {
+            Event.RegisterListener(this);
+        }
+
+        private void OnDisable()
+        {
+            Event.UnregisterListener(this);
+        }
+
+        public void OnEventRaised()
+        {
+            Response.Invoke();
+        }
+    }
+}
